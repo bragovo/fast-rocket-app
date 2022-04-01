ea0e7e660b96cc9a0cd029c746281d510dd8d5f7
Andy Braga <bragovo@gmail.com>
2022-04-01 20:49:06 +0200

    Add web app

---
+++ snowpack.config.js
@@ -0,0 +1,29 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: '/',
+    src: '/dist',
+  },
+  plugins: [
+    ['@snowpack/plugin-typescript']
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    open: "none"
+  },
+  buildOptions: {
+    /* ... */
+  },
+  routes: [
+    {
+      match: "routes",
+      src: ".*",
+      dest: "/"
+    }
+  ]
+};
