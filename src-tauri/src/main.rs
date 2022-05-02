d4753125545ca06a1871e474e67d9eeb07a5d0a8
Andy Braga <bragovo@gmail.com>
2022-05-02 16:19:56 +0200

    Add main hotkeys

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,37 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::{ Menu, MenuItem, Submenu };
+
 fn main() {
+  let about_menu = Submenu::new(
+    "Chat",
+    Menu::new()
+      .add_native_item(MenuItem::Hide)
+      // .add_native_item(MenuItem::HideOthers)
+      // .add_native_item(MenuItem::ShowAll)
+      .add_native_item(MenuItem::Separator)
+      .add_native_item(MenuItem::Quit)
+  );
+
+  let edit_menu = Submenu::new(
+    "Edit",
+    Menu::new()
+      .add_native_item(MenuItem::Undo)
+      .add_native_item(MenuItem::Redo)
+      .add_native_item(MenuItem::Separator)
+      .add_native_item(MenuItem::Cut)
+      .add_native_item(MenuItem::Copy)
+      .add_native_item(MenuItem::Paste)
+      .add_native_item(MenuItem::SelectAll)
+  );
+
+  let menu = Menu::new()
+    .add_submenu(about_menu)
+    .add_submenu(edit_menu);
+
   tauri::Builder::default()
+    .menu(menu)
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
